92975f9e2ef80fc92e62239b1d0003e2e0ec2620
Crowdbotics <team@crowdbotics.com>
2021-03-02 06:32:02 +0000

    Update models

--- backend/adsn_sports_tracker_24799/wsgi.py
+++ backend/adsn_sports_tracker_24799/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adsn_sports_tracker_24799.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adsn_sports_tracker_24799.settings")
 
 application = get_wsgi_application()
